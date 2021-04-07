025e1c7e6585682edbfb8cb2e97123d8817c0809
danmora9854 <dan.mora9854@gmail.com>
2021-04-07 15:12:25 -0600

    Create zerost.m

---
+++ zerost.m
@@ -0,0 +1,9 @@
+function zeros_t=zerost(domM)
+  s = chebfun(@(t) .5+1i*t,[5 domM]);
+    f = chebfun(@(t) zeta(s(t)),[5 domM]);
+    zeros_t_1 = roots(f,'complex','norecursion');% MS = 'markersize';
+    zeros_t_2=0.5+sqrt(-1)*zeros_t_1;
+    zeros_t=[];
+        for k=1:size(zeros_t_2)
+            zeros_t=[zeros_t ;zeros_t_2(k); 1-zeros_t_2(k)];
+end
