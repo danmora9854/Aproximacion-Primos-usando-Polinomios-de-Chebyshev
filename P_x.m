123404acbef63b0f3be9ad6bdf54357e78a961b1
danmora9854 <dan.mora9854@gmail.com>
2021-04-07 15:20:14 -0600

    Update P_x.m

--- P_x.m
+++ P_x.m
@@ -1,8 +1,8 @@
-function P=P_x(x,domM)
+function P=P_x(x,domM,zeros_t)
 N=ceil(log(x)/log(2)-1);
 b=nan(1,N);
 for k=1:N
-    b(k)=real(myJ_x(x^(1./k),domM).*moebiusmu(k)./k);
+    b(k)=real(myJ_x(x^(1./k),domM,zeros_t).*moebiusmu(k)./k);
 end
 P=sum(b);
 end
