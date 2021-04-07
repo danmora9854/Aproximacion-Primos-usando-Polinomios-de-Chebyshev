68d9324c56be9f248da5f19bc76aaed8a856ed21
danmora9854 <dan.mora9854@gmail.com>
2021-04-07 15:15:50 -0600

    Update sumzeroszeta.m

--- sumzeroszeta.m
+++ sumzeroszeta.m
@@ -1,5 +1,5 @@
-function S=sumzeroszeta(xhat,domM) 
- 
+function S=sumzeroszeta(xhat,domM,zeros_t) 
+ zeros_t=zeros_t(zeros_t<domM);
 b=[];
 for k=1:size(zeros_t)
     b(k)=li_xcomplex(xhat,zeros_t(k)); 
